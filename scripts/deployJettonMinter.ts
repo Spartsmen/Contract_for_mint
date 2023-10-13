7855f6ef140c487687b1ef3c3c1d9068c152b137
Spartsmen <aapyzhov56@gmail.com>
2023-10-13 18:34:11 +0300

    link adding

--- scripts/deployJettonMinter.ts
+++ scripts/deployJettonMinter.ts
@@ -1,15 +1,61 @@
-import { Address, beginCell, toNano } from 'ton-core';
+import { Address, beginCell, toNano, Cell } from 'ton-core';
 import { JettonMinter } from '../wrappers/JettonMinter';
 import { compile, NetworkProvider } from '@ton-community/blueprint';
 
+
 export async function run(provider: NetworkProvider) {
 
-    const randomSeed = Math.floor(Math.random() * 10000);
+    function bufferToChunks(buff: Buffer, chunkSize: number) {
+        const chunks: Buffer[] = [];
+        while (buff.byteLength > 0) {
+          chunks.push(buff.subarray(0, chunkSize));
+          buff = buff.subarray(chunkSize);
+        }
+        return chunks;
+      }
+
+      function makeSnakeCell(data: Buffer): Cell {
+        const chunks = bufferToChunks(data, 127);
+      
+        if (chunks.length === 0) {
+          return beginCell().endCell();
+        }
+      
+        if (chunks.length === 1) {
+          return beginCell().storeBuffer(chunks[0]).endCell();
+        }
+      
+        let curCell = beginCell();
+      
+        for (let i = chunks.length - 1; i >= 0; i--) {
+          const chunk = chunks[i];
+      
+          curCell.storeBuffer(chunk);
+      
+          if (i - 1 >= 0) {
+            const nextCell = beginCell();
+            nextCell.storeRef(curCell);
+            curCell = nextCell;
+          }
+        }
+      
+        return curCell.endCell();
+      }
+
+    function encodeOffChainContent(content: string) {
+        let data = Buffer.from(content);
+        const offChainPrefix = Buffer.from([0x01]);
+        data = Buffer.concat([offChainPrefix, data]);
+        return makeSnakeCell(data);
+      }
+
+    const link = 'ipfs://bafkreiast4fqlkp4upyu2cvo7fn7aabjusx765yzvqitsr4rpwfvhjguhy'; 
+    const content = encodeOffChainContent(link)
 
     const jettonMinter = provider.open(JettonMinter.createFromConfig({
         
         adminAddress: provider.sender().address as Address,
-        content: beginCell().storeUint(randomSeed, 256).endCell(),
+        content: content,
         jettonWalletCode: await compile('JettonWallet')
 
     }, await compile('JettonMinter')));
